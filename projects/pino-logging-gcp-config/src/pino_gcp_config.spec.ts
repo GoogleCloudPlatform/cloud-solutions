a1540f7b7320937374ba1a3a5c65a2351193832f
Niel Markwick <nielm@google.com>
2024-06-13 22:09:46 +0200

    Initial project creation for pino-logging-gcp-config
    Change-Id: I330f6eb161c93f6309f62948113ea3f7eb8d1266
    GitOrigin-RevId: dad6ab76a8d8cff495d040d23295cadfc89c8028

---
+++ projects/pino-logging-gcp-config/src/pino_gcp_config.spec.ts
@@ -0,0 +1,155 @@
+/**
+ * Copyright 2024 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+// eslint-disable-next-line n/no-unpublished-import
+import 'jasmine';
+import {createGcpLoggingPinoConfig, TEST_ONLY} from './pino_gcp_config';
+import * as pino from 'pino';
+
+const serviceContext = {
+  service: 'test',
+  version: '0.1.0',
+};
+
+describe('Pino config', () => {
+  const config = createGcpLoggingPinoConfig({serviceContext});
+
+  it('should add mixins to the config object', () => {
+    const pinoLoggerOptions: pino.LoggerOptions = {
+      level: 'defaultLevel',
+      formatters: {
+        bindings: x => {
+          return {a: x};
+        },
+      },
+    };
+
+    const configWithMixins = createGcpLoggingPinoConfig(
+      {serviceContext},
+      pinoLoggerOptions
+    );
+
+    expect(configWithMixins.level).toEqual('defaultLevel');
+    expect(configWithMixins.formatters?.bindings).toBeDefined();
+  });
+
+  describe('Log level conversion', () => {
+    // Need to define the return type of levelConverter because pino leaves it
+    // as a plain `object`
+    // eslint-disable-next-line @typescript-eslint/no-non-null-asserted-optional-chain
+    const levelConverter = config.formatters?.level! as (
+      label: string,
+      level: number
+    ) => Record<string, unknown>;
+
+    Object.entries(TEST_ONLY.PINO_TO_GCP_LOG_LEVELS).forEach(e => {
+      it(`converts pino level ${e[0]} to GCP ${e[1]}`, () => {
+        const convertedLevel = levelConverter(e[0], 42);
+
+        expect(convertedLevel.severity).toEqual(e[1]);
+      });
+    });
+
+    it('defaults to INFO for an unknown level', () => {
+      expect(levelConverter('blah', 66).severity).toEqual('INFO');
+    });
+  });
+
+  describe('log formatter', () => {
+    // eslint-disable-next-line @typescript-eslint/no-non-null-asserted-optional-chain
+    const formatLogObject = config.formatters?.log!;
+    const TEST_DATE = new Date('2024-04-13T16:12:34.123');
+
+    beforeEach(() => {
+      jasmine.clock().install();
+      jasmine.clock().mockDate(TEST_DATE);
+    });
+
+    afterEach(() => {
+      jasmine.clock().uninstall();
+    });
+
+    it('should add serviceContext', () => {
+      const logObject = formatLogObject({});
+
+      expect(logObject.serviceContext).toEqual({
+        service: 'test',
+        version: '0.1.0',
+      });
+    });
+
+    it('should add insertId', () => {
+      const logObject = formatLogObject({});
+
+      expect(logObject['logging.googleapis.com/insertId']).toBeDefined();
+      expect(
+        (logObject['logging.googleapis.com/insertId'] as string).length
+      ).toBeGreaterThan(1);
+    });
+
+    it('should add stack_trace when err is an Error', () => {
+      const logObject = formatLogObject({
+        err: new Error('some error message'),
+        message: 'thrown an exception',
+      });
+
+      expect(logObject.stack_trace).toMatch('Error: some error message\n');
+    });
+
+    it('should not add stack_trace when err is not an Error', () => {
+      const logObject = formatLogObject({
+        err: 'some error message',
+        message: 'thrown a string as an exception',
+      });
+
+      expect(logObject.stack_trace).toBeUndefined();
+    });
+
+    it('should not map span and trace properties when not present', () => {
+      const formattedLog = formatLogObject({message: 'hello'});
+
+      expect(
+        formattedLog['logging.googleapis.com/trace_sampled']
+      ).toBeUndefined();
+      expect(formattedLog['logging.googleapis.com/trace']).toBeUndefined();
+      expect(formattedLog['logging.googleapis.com/spanId']).toBeUndefined();
+    });
+
+    it('should replace span and trace properties when not present', () => {
+      const formattedLog = formatLogObject({
+        message: 'hello',
+        trace_id: 'trace:12345',
+        span_id: 'span:23456',
+        trace_flags: 1,
+      });
+
+      expect(formattedLog['logging.googleapis.com/trace_sampled']).toBeTrue();
+      expect(formattedLog['logging.googleapis.com/trace']).toBe('trace:12345');
+      expect(formattedLog['logging.googleapis.com/spanId']).toBe('span:23456');
+      expect(formattedLog.trace_id).toBeUndefined();
+      expect(formattedLog.span_id).toBeUndefined();
+      expect(formattedLog.trace_flags).toBeUndefined();
+    });
+
+    it('adds a timestamp as seconds:nanos JSON fragment', () => {
+      const timestampGenerator = config.timestamp as () => string;
+
+      expect(timestampGenerator()).toEqual(
+        ',"timestamp":{"seconds":1713017554,"nanos":122000000}'
+      );
+    });
+  });
+});
---
+++ projects/pino-logging-gcp-config/src/pino_gcp_config.ts
@@ -0,0 +1,307 @@
+/**
+ * Copyright 2024 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * @fileoverview Create a default LoggerConfig for pino structured logging.
+ *
+ * @see https://cloud.google.com/logging/docs/structured-logging
+ */
+
+import * as pino from 'pino';
+import {EventId} from 'eventid';
+import {
+  Logging,
+  ServiceContext,
+  detectServiceContext,
+} from '@google-cloud/logging';
+import {
+  setInstrumentationStatus,
+  createDiagnosticEntry,
+} from '@google-cloud/logging/build/src/utils/instrumentation';
+import * as gax from 'google-gax';
+
+/** Monotonically increasing ID for insertId. */
+const eventId = new EventId();
+
+/*
+ * Uses release-please annotations to update with latest version.
+ * See
+ * https://github.com/googleapis/release-please/blob/main/docs/customizing.md#updating-arbitrary-files
+ */
+const NODEJS_GCP_PINO_LIBRARY_VERSION = '1.0.0'; // {x-release-please-version}
+const NODEJS_GCP_PINO_LIBRARY_NAME = 'nodejs-gcp-pino';
+
+const PINO_TO_GCP_LOG_LEVELS = Object.freeze(
+  Object.fromEntries([
+    ['trace', 'DEBUG'],
+    ['debug', 'DEBUG'],
+    ['info', 'INFO'],
+    ['warn', 'WARNING'],
+    ['error', 'ERROR'],
+    ['fatal', 'CRITICAL'],
+  ])
+) as Record<pino.Level, string>;
+
+/**
+ * Parameters for configuring GCP logging for Pino, allows specifying
+ * serviceContext for Error Reporting.
+ */
+export interface GCPLoggingPinoOptions {
+  /**
+   * Optional details of service to be logged, and used in Cloud Error
+   * Reporting.
+   *
+   * If specified, the {@link ServiceContext.service} name must be given.
+   *
+   * If not specified, a service name will be auto-detected from the
+   * environment.
+   *
+   * @see https://cloud.google.com/error-reporting/docs/formatting-error-messages
+   *
+   */
+  serviceContext?: ServiceContext;
+
+  /**
+   * Optional GoogleAuth - used to override defaults when detecting
+   * ServiceContext from the environment.
+   */
+  auth?: gax.GoogleAuth;
+}
+
+/**
+ * Encapsulates configuration and methods for formatting pino logs for Google
+ * Cloud Logging.
+ */
+class GcpLoggingPino {
+  serviceContext: ServiceContext | null = null;
+
+  constructor(options: GCPLoggingPinoOptions) {
+    if (options.serviceContext) {
+      if (
+        typeof options.serviceContext?.service !== 'string' ||
+        !options.serviceContext?.service?.length
+      ) {
+        throw new Error('options.serviceContext.service must be specified.');
+      }
+      this.serviceContext = {...options.serviceContext};
+      this.outputDiagnosticEntry();
+    } else {
+      // Use the Cloud Logging libraries to retrieve the ServiceContext
+      // automatically from the environment.
+      //
+      // This requires initialising a Cloud Logger, then using
+      // detectServiceContext to asynchronously return the ServiceContext
+      const cloudLog = new Logging({auth: options.auth}).logSync(
+        NODEJS_GCP_PINO_LIBRARY_NAME
+      );
+
+      detectServiceContext(cloudLog.logging.auth).then(
+        serviceContext => {
+          this.serviceContext = serviceContext;
+          this.outputDiagnosticEntry();
+        },
+        () => {
+          // Ignore any errors raised by detectServiceContext.
+          // Errors can occur if not running in a GCP environment.
+        }
+      );
+    }
+  }
+
+  /**
+   * Outputs a single log line once per process containing the diagnostic info
+   * for this logger.
+   */
+  outputDiagnosticEntry() {
+    const alreadyWritten = setInstrumentationStatus(true);
+    if (alreadyWritten) {
+      return;
+    }
+    const instrumentationEntry = createDiagnosticEntry(
+      NODEJS_GCP_PINO_LIBRARY_NAME,
+      NODEJS_GCP_PINO_LIBRARY_VERSION
+    );
+    // Create a temp pino logger instance just to log this diagnostic entry.
+    pino
+      .pino({
+        level: 'info',
+        ...this.getPinoLoggerOptions(),
+      })
+      .info({
+        ...instrumentationEntry.data,
+      });
+  }
+
+  /**
+   * Creates a JSON fragment string containing the timestamp in GCP logging
+   * format.
+   *
+   * @example ', "timestamp": { "seconds": 123456789, "nanos": 123000000 }'
+   *
+   * Creating a string with seconds/nanos is ~10x faster than formatting the
+   * timestamp as an ISO string.
+   *
+   * @see https://cloud.google.com/logging/docs/agent/logging/configuration#timestamp-processing
+   *
+   * As Javascript Date uses millisecond precision, in
+   * {@link formatLogObject} the logger adds a monotonically increasing insertId
+   * into the log object to preserve log order inside GCP logging.
+   *
+   * @see https://github.com/googleapis/nodejs-logging/blob/main/src/entry.ts#L189
+   */
+  static getGcpLoggingTimestamp() {
+    const seconds = Date.now() / 1000;
+    const secondsRounded = Math.floor(seconds);
+    const millis = Math.floor((seconds - secondsRounded) * 1000);
+    return `,"timestamp":{"seconds":${secondsRounded},"nanos":${millis}000000}`;
+  }
+
+  /**
+   * Converts pino log level to Google severity level.
+   *
+   * @see pino.LoggerOptions.formatters.level
+   */
+  static pinoLevelToGcpSeverity(
+    pinoSeverityLabel: string,
+    pinoSeverityLevel: number
+  ): Record<string, unknown> {
+    const pinoLevel = pinoSeverityLabel as pino.Level;
+    const severity = PINO_TO_GCP_LOG_LEVELS[pinoLevel] ?? 'INFO';
+    return {
+      severity,
+      level: pinoSeverityLevel,
+    };
+  }
+
+  /**
+   * Reformats log entry record for GCP.
+   *
+   * * Adds OpenTelemetry properties with correct key.
+   * * Adds stack_trace if an Error is given in the err property.
+   * * Adds serviceContext
+   * * Adds sequential insertId to preserve logging order.
+   */
+  formatLogObject(entry: Record<string, unknown>): Record<string, unknown> {
+    // OpenTelemetry adds properties trace_id, span_id, trace_flags. If these
+    // are present, not null and not blank, convert them to the property keys
+    // specified by GCP logging.
+    //
+    // @see https://cloud.google.com/logging/docs/structured-logging#special-payload-fields
+    // @see https://github.com/open-telemetry/opentelemetry-specification/blob/main/specification/logs/data-model.md#trace-context-fields
+    if ((entry.trace_id as string | undefined)?.length) {
+      entry['logging.googleapis.com/trace'] = entry.trace_id;
+      delete entry.trace_id;
+    }
+    if ((entry.span_id as string | undefined)?.length) {
+      entry['logging.googleapis.com/spanId'] = entry.span_id;
+      delete entry.span_id;
+    }
+    // Trace flags is a bit field even though there is one on defined bit,
+    // so lets convert it to an int and test against a bitmask.
+    // @see https://www.w3.org/TR/trace-context/#trace-flags
+    const trace_flags_bits = parseInt(entry.trace_flags as string);
+    if (!!trace_flags_bits && (trace_flags_bits & 0x1) === 1) {
+      entry['logging.googleapis.com/trace_sampled'] = true;
+    }
+    delete entry.trace_flags;
+
+    if (this.serviceContext) {
+      entry.serviceContext = this.serviceContext;
+    }
+
+    // If there is an Error, add the stack trace for Event Reporting.
+    if (entry.err instanceof Error && entry.err.stack) {
+      entry.stack_trace = entry.err.stack;
+    }
+
+    // Add a sequential EventID.
+    //
+    // This is required because Javascript Date has a very coarse granularity
+    // (millisecond), which makes it quite likely that multiple log entries
+    // would have the same timestamp.
+    //
+    // The GCP Logging API doesn't guarantee to preserve insertion order for
+    // entries with the same timestamp. The service does use `insertId` as a
+    // secondary ordering for entries with the same timestamp. `insertId` needs
+    // to be globally unique (within the project) however.
+    //
+    // We use a globally unique monotonically increasing EventId as the
+    // insertId.
+    //
+    // @see https://github.com/googleapis/nodejs-logging/blob/main/src/entry.ts#L189
+    entry['logging.googleapis.com/insertId'] = eventId.new();
+
+    return entry;
+  }
+
+  /**
+   * Creates a pino.LoggerOptions configured for GCP structured logging.
+   */
+  getPinoLoggerOptions(
+    pinoOptionsMixin?: pino.LoggerOptions
+  ): pino.LoggerOptions {
+    const formattersMixin = pinoOptionsMixin?.formatters;
+    return {
+      ...pinoOptionsMixin,
+      // Use 'message' instead of 'msg' as log entry message key.
+      messageKey: 'message',
+      formatters: {
+        ...formattersMixin,
+        level: GcpLoggingPino.pinoLevelToGcpSeverity,
+        log: (entry: Record<string, unknown>) => this.formatLogObject(entry),
+      },
+      timestamp: () => GcpLoggingPino.getGcpLoggingTimestamp(),
+    };
+  }
+}
+
+/**
+ * Creates a {@link pino.LoggerOptions} object which configures pino to output
+ * JSON records compatible with
+ * {@link https://cloud.google.com/logging/docs/structured-logging|Google Cloud structured Logging}.
+ *
+ * @param pinoLoggerOptions Optional pino Logger settings that will be added to
+ * the returned value.
+ *
+ * @example
+ *      const logger = pino.pino(
+ *        createGcpLoggingPinoConfig(
+ *          {
+ *            serviceContext: {
+ *              service: 'my-service',
+ *              version: '1.2.3',
+ *            },
+ *          },
+ *          {
+ *            // Set Pino log level to 'debug'.
+ *            level: 'debug',
+ *          }
+ *        )
+ *      );
+ *
+ *      logger.info('hello world');
+ *      logger.error(err, 'failure: ' + err);
+ */
+export function createGcpLoggingPinoConfig(
+  options: GCPLoggingPinoOptions,
+  pinoLoggerOptions?: pino.LoggerOptions
+): pino.LoggerOptions {
+  return new GcpLoggingPino(options).getPinoLoggerOptions(pinoLoggerOptions);
+}
+
+export const TEST_ONLY = {
+  PINO_TO_GCP_LOG_LEVELS,
+};
